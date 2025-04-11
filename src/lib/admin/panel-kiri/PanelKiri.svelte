d425bc7fc361a979119fc9b44cf14eec2867d174
maszhar <fikrimustofa024@gmail.com>
2025-04-12 01:53:38 +0700

    atur z-index

--- src/lib/admin/panel-kiri/PanelKiri.svelte
+++ src/lib/admin/panel-kiri/PanelKiri.svelte
@@ -2,7 +2,7 @@
 	import ItemPanelKiri from './ItemPanelKiri.svelte';
 </script>
 
-<div class="fixed top-0 left-0 h-full w-48 pt-12">
+<div class="fixed top-0 left-0 z-30 h-full w-48 pt-12">
 	<div class="flex h-full flex-col gap-1 overflow-y-auto bg-zinc-800 p-2 text-white">
 		<ItemPanelKiri href="/admin/artikel">Artikel</ItemPanelKiri>
 		<ItemPanelKiri href="/admin/kategori">Kategori</ItemPanelKiri>
