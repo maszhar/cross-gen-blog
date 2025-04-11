35fc3bde723b9c1f496e3624a3af0b070b9f146b
maszhar <fikrimustofa024@gmail.com>
2025-04-11 19:47:41 +0700

    inisiasi proyek dan buat navbar

---
+++ src/lib/customer/navbar/Navbar.svelte
@@ -0,0 +1,3 @@
+<nav class="fixed top-0 left-0 flex h-12 w-full items-center bg-zinc-800 px-12 text-white">
+	<h3 class="font-sans text-3xl font-bold tracking-wide">MZHR</h3>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Navbar from '$lib/customer/navbar/Navbar.svelte';
+</script>
+
+<Navbar />
