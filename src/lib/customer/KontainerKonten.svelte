2473aa5375a0b5fbb7509139e22b601af95690f1
maszhar <fikrimustofa024@gmail.com>
2025-04-15 13:01:18 +0700

    perkecil padding kontainer di perangkat mobile

--- src/lib/customer/KontainerKonten.svelte
+++ src/lib/customer/KontainerKonten.svelte
@@ -8,6 +8,6 @@
 	const { children, class: className = '' }: Properti = $props();
 </script>
 
-<div class="rounded border border-zinc-300 p-8 {className}">
+<div class="rounded border border-zinc-300 p-5 sm:p-8 {className}">
 	{@render children?.()}
 </div>
