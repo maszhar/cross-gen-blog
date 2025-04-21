2dbacc95e2be095359a48d8cde32227fbb1be808
maszhar <fikrimustofa024@gmail.com>
2025-04-21 11:17:25 +0700

    implement Google Analytics tracking with dynamic gtag ID

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,7 @@ import type { LayoutServerLoad } from './$types';
 import { RepositoriKategori } from '$lib/common/data/RepositoriKategori';
 import type { Kategori } from '$lib/common/entitas/Kategori';
 import { PUBLIC_SITE_URL } from '$env/static/public';
+import { env } from '$env/dynamic/private';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	const db: Connection = (locals as any).db;
@@ -21,10 +22,12 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 	}
 
 	const canonical = PUBLIC_SITE_URL + url.pathname;
+	const gtagId = env.GTAG_ID;
 
 	return {
 		dataKoleksiKategoriFooter: koleksiKategori.map((kategori) => kategori.serialize()),
 		kategoriFooterMasihAda: kategoriMasihAda,
-		canonical
+		canonical,
+		gtagId
 	};
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,17 @@
 
 <svelte:head>
 	<link rel="canonical" href={data.canonical} />
+	{#if data.gtagId}
+		<script async src={`https://www.googletagmanager.com/gtag/js?id=${data.gtagId}`}></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				window.dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+			gtag('config', data.gtagId, { page_path: location.pathname });
+		</script>
+	{/if}
 </svelte:head>
 
 {@render children()}
